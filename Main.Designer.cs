3550a3da7027119108b40aa934546ebad4942da4
emmanetan <emmannate283@gmail.com>
2025-05-04 15:10:07 +0800

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CSCISystem1._1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new AntdUI.Panel();
+            this.panel2 = new AntdUI.Panel();
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.siticoneButton1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Radius = 20;
+            this.panel1.Size = new System.Drawing.Size(267, 696);
+            this.panel1.TabIndex = 0;
+            this.panel1.Text = "panel1";
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(277, 10);
+            this.panel2.Name = "panel2";
+            this.panel2.padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.panel2.Radius = 20;
+            this.panel2.Size = new System.Drawing.Size(1001, 696);
+            this.panel2.TabIndex = 1;
+            this.panel2.Text = "panel2";
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(48, 156);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(155, 45);
+            this.siticoneButton1.TabIndex = 0;
+            this.siticoneButton1.Text = "siticoneButton1";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(234)))), ((int)(((byte)(253)))));
+            this.ClientSize = new System.Drawing.Size(1288, 716);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Main";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AntdUI.Panel panel1;
+        private AntdUI.Panel panel2;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+    }
+}
+
