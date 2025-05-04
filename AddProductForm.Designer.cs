fe468e9c5cd6653c9a74c647726e2dc129d0732f
emmanetan <emmannate283@gmail.com>
2025-05-04 23:59:39 +0800

    SS

--- AddProductForm.Designer.cs
+++ AddProductForm.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new AntdUI.Panel();
+            this.input1 = new AntdUI.Input();
+            this.input2 = new AntdUI.Input();
+            this.input3 = new AntdUI.Input();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.input3);
+            this.panel1.Controls.Add(this.input2);
+            this.panel1.Controls.Add(this.input1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 430);
+            this.panel1.TabIndex = 0;
+            this.panel1.Text = "panel1";
+            // 
+            // input1
+            // 
+            this.input1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.input1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input1.IconGap = 1F;
+            this.input1.IconRatio = 1F;
+            this.input1.Location = new System.Drawing.Point(103, 49);
+            this.input1.Name = "input1";
+            this.input1.PlaceholderText = "Search";
+            this.input1.Radius = 20;
+            this.input1.Size = new System.Drawing.Size(577, 42);
+            this.input1.TabIndex = 2;
+            this.input1.WaveSize = 1;
+            // 
+            // input2
+            // 
+            this.input2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.input2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input2.IconGap = 1F;
+            this.input2.IconRatio = 1F;
+            this.input2.Location = new System.Drawing.Point(103, 97);
+            this.input2.Name = "input2";
+            this.input2.PlaceholderText = "Search";
+            this.input2.Radius = 20;
+            this.input2.Size = new System.Drawing.Size(577, 42);
+            this.input2.TabIndex = 3;
+            this.input2.WaveSize = 1;
+            // 
+            // input3
+            // 
+            this.input3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.input3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input3.IconGap = 1F;
+            this.input3.IconRatio = 1F;
+            this.input3.Location = new System.Drawing.Point(103, 145);
+            this.input3.Name = "input3";
+            this.input3.PlaceholderText = "Search";
+            this.input3.Radius = 20;
+            this.input3.Size = new System.Drawing.Size(577, 42);
+            this.input3.TabIndex = 4;
+            this.input3.WaveSize = 1;
+            // 
+            // AddProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(240)))), ((int)(((byte)(244)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "AddProductForm";
+            this.Padding = new System.Windows.Forms.Padding(10);
             this.Text = "AddProductForm";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private AntdUI.Panel panel1;
+        private AntdUI.Input input3;
+        private AntdUI.Input input2;
+        private AntdUI.Input input1;
     }
 }
